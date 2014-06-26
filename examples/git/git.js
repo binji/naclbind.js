2048ee26cfff73990faa41ebfe401fd86569dcf1
Ben Smith <binji@chromium.org>
2014-06-26 15:51:08 -0700

    Add more to the git example.
    It fails when trying to create the signature. I also added giterr_last
    to extract the most recent error.

--- examples/git/git.js
+++ examples/git/git.js
@@ -21,19 +21,42 @@ require(['promise', 'nacl', 'git_glue'], function(promise, nacl, git_glue) {
 
   var c = m.makeContext();
   var dirname = '/';
+  var repo;
+  var sig;
+  var index;
+  var tree_id, commit_id;
+  var tree;
 
   function dostuff() {
     promise.resolve().then(function() {
       f.git_threads_init(c);
       var lenPtr = m.mallocType(c, t.uint32);
       var dirnameCstr = f.varToUtf8(c, dirname, lenPtr);
-      var repo = m.mallocType(c, t.git_repository$);
+      repo = m.mallocType(c, t.git_repository$);
       var result = f.git_repository_init(c, repo, dirnameCstr, 0);
       return m.commitPromise(result);
     }).then(function(result) {
       console.log('git_repository_init returned ' + result);
+
+      repo = f.get(c, repo).cast(t.git_repository$);
+
+      sig = m.mallocType(c, t.git_signature$);
+      var result = f.git_signature_default(c, sig, repo);
+      return m.commitPromise(result);
+    }).then(function(result) {
+      if (result < 0) {
+        var err = f.giterr_last(c);
+        var message = m.getField(c, t._git_error.fields.message, err);
+        f.puts(c, message);
+        return m.commitPromise();
+      }
     }).catch(function(err) {
       console.error('error: ' + err.stack);
+    }).finally(function() {
+      f.git_repository_free(c, repo);
+      f.git_threads_shutdown(c);
+      m.destroyHandles(c);
+      return m.commitPromise();
     });
   }
 
