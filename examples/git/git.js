ff5de82fe22a1a0e7da25c79d298a34c3ff344ff
Ben Smith <binji@chromium.org>
2014-06-30 16:28:27 -0700

    Finish git init example

--- examples/git/git.js
+++ examples/git/git.js
@@ -27,6 +27,13 @@ require(['promise', 'nacl', 'git_glue'], function(promise, nacl, git_glue) {
   var tree_id, commit_id;
   var tree;
 
+  function writeError() {
+    var err = f.giterr_last(c);
+    var message = m.getField(c, t._git_error.fields.message, err);
+    f.puts(c, message);
+    return m.commitPromise();
+  }
+
   function dostuff() {
     promise.resolve().then(function() {
       f.git_threads_init(c);
@@ -34,20 +41,68 @@ require(['promise', 'nacl', 'git_glue'], function(promise, nacl, git_glue) {
       var result = f.git_repository_init(c, repo, dirname, 0);
       return m.commitPromise(result);
     }).then(function(result) {
-      console.log('git_repository_init returned ' + result);
+      if (result < 0) {
+        return writeError();
+      }
 
       repo = f.get(c, repo).cast(t.git_repository$);
 
       sig = m.mallocType(c, t.git_signature$);
-      var result = f.git_signature_default(c, sig, repo);
+      var result = f.git_signature_now(c, sig, "Foo Bar", "foobar@example.com");
       return m.commitPromise(result);
     }).then(function(result) {
       if (result < 0) {
-        var err = f.giterr_last(c);
-        var message = m.getField(c, t._git_error.fields.message, err);
-        f.puts(c, message);
-        return m.commitPromise();
+        return writeError();
       }
+
+      sig = f.get(c, sig).cast(t.git_signature$);
+
+      index = m.mallocType(c, t.git_index$);
+      var result = f.git_repository_index(c, index, repo);
+      return m.commitPromise(result);
+    }).then(function(result) {
+      if (result < 0) {
+        return writeError();
+      }
+
+      index = f.get(c, index).cast(t.git_index$);
+      tree_id = m.mallocType(c, t.git_oid);
+      var result = f.git_index_write_tree(c, tree_id, index);
+      return m.commitPromise(result);
+    }).then(function(result) {
+      if (result < 0) {
+        return writeError();
+      }
+
+      f.git_index_free(c, index);
+    }).then(function(result) {
+      if (result < 0) {
+        return writeError();
+      }
+
+      tree = m.mallocType(c, t.git_tree$);
+      var result = f.git_tree_lookup(c, tree, repo, tree_id);
+      return m.commitPromise(result);
+    }).then(function(result) {
+      if (result < 0) {
+        return writeError();
+      }
+
+      tree = f.get(c, tree).cast(t.git_tree$);
+
+      commit_id = m.mallocType(c, t.git_oid);
+      var result = f.git_commit_create(c, commit_id, repo, "HEAD", sig, sig,
+                                       null, "Initial Commit", tree, 0, null);
+
+      return m.commitPromise(result);
+    }).then(function(result) {
+      if (result < 0) {
+        return writeError();
+      }
+
+      f.git_tree_free(c, tree);
+      f.git_signature_free(c, sig);
+      return m.commitPromise();
     }).catch(function(err) {
       console.error('error: ' + err.stack);
     }).finally(function() {
