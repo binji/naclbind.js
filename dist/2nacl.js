b947256b206afe77042d9da6a5ef8a72bac6b4e7
Ben Smith <binji@chromium.org>
2014-07-28 13:09:44 -0700

    Use browserify from node_modules.
    This is nicer than requiring the developer to install browserify
    globally.

--- dist/2nacl.js
+++ dist/2nacl.js
@@ -1,4 +1,4 @@
-!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.tonacl=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.tonacl=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 // Copyright 2014 Ben Smith. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -396,7 +396,7 @@ module.exports = {
   init: init
 };
 
-},{}],2:[function(_dereq_,module,exports){
+},{}],2:[function(require,module,exports){
 // Copyright 2014 Ben Smith. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -413,7 +413,7 @@ module.exports = {
 
 "use strict";
 
-var builtin = _dereq_('./builtin');
+var builtin = require('./builtin');
 
 function assert(cond, msg) {
   if (!cond) {
@@ -1534,6 +1534,5 @@ module.exports = {
   Module: Module
 };
 
-},{"./builtin":1}]},{},[2])
-(2)
+},{"./builtin":1}]},{},[2])(2)
 });
