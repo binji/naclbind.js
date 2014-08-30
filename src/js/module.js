8099ff5cab6d6715208519562fe948fb4331b45a
Ben Smith <binji@chromium.org>
2014-08-30 10:28:49 -0700

    Add module.js and new test
    This probably will change a bit, but it's good for now.

---
+++ src/js/module.js
@@ -0,0 +1,212 @@
+// Copyright 2014 Ben Smith. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var type = require('./type'),
+    utils = require('./utils');
+
+function numberToType(n) {
+  if (!isFinite(n) || !utils.isInteger(n)) {
+    if (utils.isFloat(n)) {
+      return type.float;
+    } else {
+      return type.double;
+    }
+  } else if (n < 0) {
+    // Use the smallest integer type possible.
+    if (n >= utils.S8_MIN) {
+      return type.schar;
+    } else if (n >= utils.S16_MIN) {
+      return type.short;
+    } else {
+      return type.int;
+    }
+  } else {
+    if (n <= utils.S8_MAX) {
+      return type.schar;
+    } else if (n <= utils.U8_MAX) {
+      return type.uchar;
+    } else if (n <= utils.S16_MAX) {
+      return type.short;
+    } else if (n <= utils.U16_MAX) {
+      return type.ushort;
+    } else {
+      return type.int;
+    }
+  }
+}
+
+function objectToType(obj) {
+  var klass = utils.getClass(obj);
+  switch (klass) {
+    case 'Number':
+      return numberToType(obj);
+    case 'String':
+      return type.Pointer(type.char.qualify(type.CONST));
+    // TODO(binji): handle other JS types.
+    default:
+      throw new Error('Unknown JavaScript class: "' + klass + '".');
+  }
+}
+
+function objectToHandle(context, obj) {
+  var type = objectToType(obj);
+  return context.createHandle(type, obj);
+}
+
+function argsToHandles(context, args) {
+  return Array.prototype.map.call(args, function(arg) {
+    return (arg instanceof Handle) ?  arg : objectToHandle(context, arg);
+  });
+}
+
+function Module() {
+  if (!(this instanceof Module)) { return new Module(); }
+  this.$handles_ = new HandleList();
+  this.$functions_ = {};
+  this.$context = this.$createContext();
+  this.$initMessage_();
+}
+Module.prototype.$defineFunction = function(name, functions) {
+  utils.checkArray(functions, IdFunction);
+
+  if (this.$functions_[name] !== undefined) {
+    throw new Error('Function named "' + name + '" is already defined.');
+  }
+
+  var self = this,
+      getType = function(x) { return x.type; },
+      fnTypes = Array.prototype.map.call(functions, getType);
+
+  this.$functions_[name] = functions;
+  this[name] = function() {
+    var argHandles = argsToHandles(self.$context, arguments),
+        argTypes = argHandles.map(getType),
+        bestFnIdx = type.getBestViableFunction(fnTypes, argTypes),
+        fn,
+        retHandle;
+
+    if (bestFnIdx < 0) {
+      // TODO(binji): print nice error here.
+      throw new Error('Call to "' + name + '" failed. ArgTypes: ' +
+                      argTypes.map(type.getSpelling).join(', '));
+    }
+
+    fn = functions[bestFnIdx];
+    if (fn.type.resultType !== type.void) {
+      retHandle = self.$context.createHandle(fn.type.resultType);
+    }
+
+    self.$registerHandlesWithValues_(argHandles);
+    self.$pushCommand_(fn.id, argHandles, retHandle);
+
+    return retHandle;
+  };
+};
+Module.prototype.$createContext = function() {
+  return new Context(this.$handles_);
+};
+Module.prototype.$initMessage_ = function() {
+  this.$message_ = {handles: {}, commands:[]};
+};
+Module.prototype.$getMessage = function() {
+  return this.$message_;
+};
+Module.prototype.$registerHandlesWithValues_ = function(handles) {
+  var i;
+  for (i = 0; i < handles.length; ++i) {
+    if (handles[i].value === null) {
+      continue;
+    }
+
+    this.$message_.handles[handles[i].id] = handles[i].value;
+  }
+};
+Module.prototype.$pushCommand_ = function(id, argHandles, retHandle) {
+  var command = {
+    id: id,
+    args: argHandles.map(function(x) { return x.id; })
+  };
+
+  if (retHandle) {
+    command.ret = retHandle.id;
+  }
+
+  this.$message_.commands.push(command);
+};
+
+function IdFunction(id, fnType) {
+  if (!(this instanceof IdFunction)) { return new IdFunction(id, fnType); }
+  utils.checkNonnegativeNumber(id);
+  type.checkType(fnType, 'type', type.FUNCTIONPROTO);
+
+  this.id = id;
+  this.type = fnType;
+}
+
+function HandleList() {
+  this.nextId_ = 1;
+  this.idToHandle_ = {};
+}
+HandleList.prototype.createHandle = function(context, type, value, id) {
+  if (id === undefined) {
+    id = this.nextId_++;
+  }
+
+  return new Handle(context, type, value, id);
+};
+HandleList.prototype.get = function(id) {
+  return this.idToHandle_[id];
+};
+HandleList.prototype.registerHandle = function(handle) {
+  this.idToHandle_[handle.id] = handle;
+};
+
+function Context(handleList) {
+  this.handleList = handleList;
+  this.handles = [];
+}
+Context.prototype.createHandle = function(type, value, id) {
+  return this.handleList.createHandle(this, type, value, id);
+};
+Context.prototype.registerHandle = function(handle) {
+  this.handleList.registerHandle(handle);
+  this.handles.push(handle);
+};
+
+function Handle(context, type, value, id) {
+  this.id = id;
+  this.type = type;
+  this.value = value || null;
+  this.context = context;
+  this.context.registerHandle(this);
+}
+Handle.prototype.cast = function(type) {
+  var castResult = this.type.canCastTo(type);
+  if (castResult === type.CAST_ERROR) {
+    throw new Error('Invalid cast: ' + this.type.spelling + ' to ' +
+                    type.spelling + '.');
+  }
+
+  return this.context.handleList.createHandle(this.context, type, this.value,
+                                              this.id);
+};
+
+
+module.exports = {
+  Module: Module,
+  Function: IdFunction,
+
+  numberToType: numberToType,
+  objectToType: objectToType,
+};
--- test/test_module.js
+++ test/test_module.js
@@ -12,235 +12,40 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var naclbind = require('../dist/naclbind'),
-    EmbedElement = require('./test_embed_element'),
-    assert = require('assert');
+var assert = require('assert'),
+    module = require('../src/js/module.js'),
+    type = require('../src/js/type.js');
 
-function doNothing() {}
-
-/*
 describe('Module', function() {
-  var m, t, el;
-  beforeEach(function() {
-    m = naclbind.Module('foo.nmf', 'application/x-pnacl', {
-      name: 'foo',
-      embedElementConstructor: EmbedElement,
-      log: doNothing,
+  it('should look like dis', function() {
+    var voidp = type.Pointer(type.void),
+        intp = type.Pointer(type.int),
+        mallocType = type.Function(voidp, [type.uint]),
+        getIntType = type.Function(type.int, [intp]),
+        m = module.Module(),
+        p,
+        x;
+
+    m.$defineFunction('malloc', [module.Function(1, mallocType)]);
+    m.$defineFunction('get', [module.Function(2, getIntType)]);
+
+    assert.deepEqual(m.$getMessage(), {handles:{}, commands:[]});
+
+    p = m.malloc(4).cast(intp);
+
+    assert.deepEqual(m.$getMessage(), {
+      handles: { 1: 4 },
+      commands: [ {id: 1, args: [1], ret: 2} ]
     });
 
-    el = m.element;
-    t = m.types;
-  });
+    x = m.get(p);
 
-  it('should not post messages before it is loaded', function() {
-    el.setPostMessageCallback(function(msg) {
-      assert.fail(undefined, undefined, "Unexpected postMessage");
-    });
-    m.commit(doNothing);
-  });
-
-  it('should post queued messages after it is loaded', function(done) {
-    el.setPostMessageCallback(function(msg) {
-      assert.equal(el.loaded, true);
-      done();
-    });
-    m.commit(doNothing);
-    el.load();
-  });
-
-  describe('commit', function() {
-    it('should throw without a callback', function() {
-      el.setPostMessageCallback(doNothing);
-      el.load();
-      assert.throws(function() { m.commit(); });
-    });
-
-    it('should throw if called with non-handle', function() {
-      el.setPostMessageCallback(doNothing);
-      el.load();
-      assert.throws(function() { m.commit(0, doNothing); });
-    });
-
-    it('should post message (0 cmds)', function(done) {
-      el.load();
-      el.setPostMessageCallback(function(msg) {
-        assert.deepEqual(msg, {
-          id: 1,
-          commands: [],
-          handles: []
-        });
-        done();
-      });
-      m.commit(doNothing);
-    });
-
-    it('should post message (1 cmd)', function(done) {
-      el.load();
-      el.setPostMessageCallback(function(msg) {
-        assert.deepEqual(msg, {
-          id: 1,
-          commands: [{
-            cmd: 'add',
-            type: msg.commands[0].type,
-            args: [3, 4],
-            argIsHandle: [false, false],
-            ret: 1
-          }],
-          handles: []
-        });
-        done();
-      });
-
-      var c = m.makeContext();
-      c.add(3, 4);
-      m.commit(doNothing);
-    });
-
-    it('should post message (1 cmd, 1 handle)', function(done) {
-      el.load();
-      el.setPostMessageCallback(function(msg) {
-        assert.deepEqual(msg, {
-          id: 1,
-          commands: [{
-            cmd: 'add',
-            type: msg.commands[0].type,
-            args: [3, 4],
-            argIsHandle: [false, false],
-            ret: 1
-          }],
-          handles: [1]
-        });
-        done();
-      });
-
-      var c = m.makeContext();
-      var sum = c.add(3, 4);
-      assert.equal(sum.id, 1);
-      m.commit(sum, doNothing);
-    });
-
-    it('should post message (2 cmds, 2 handles)', function(done) {
-      el.load();
-      el.setPostMessageCallback(function(msg) {
-        assert.deepEqual(msg, {
-          id: 1,
-          commands: [{
-            cmd: 'add',
-            type: msg.commands[0].type,
-            args: [3, 4],
-            argIsHandle: [false, false],
-            ret: 1
-          }, {
-            cmd: 'sub',
-            type: msg.commands[1].type,
-            args: [1, 5],
-            argIsHandle: [true, false],
-            ret: 2
-          }],
-          handles: [1, 2]
-        });
-        done();
-      });
-
-      var c = m.makeContext();
-      var sum1 = c.add(3, 4);
-      var sum2 = c.sub(sum1, 5);
-      assert.equal(sum1.id, 1);
-      assert.equal(sum2.id, 2);
-      m.commit(sum1, sum2, doNothing);
-    });
-
-
-    it('should call commit callback after response', function(done) {
-      var gotPostMessage = false;
-      el.load();
-      el.setPostMessageCallback(function(msg) {
-        gotPostMessage = true;
-        el.message({id: msg.id, values: []});
-      });
-      m.commit(function() {
-        assert(gotPostMessage);
-        done();
-      });
-    });
-
-    it('should respond with handle values', function(done) {
-      el.load();
-      el.setPostMessageCallback(function(msg) {
-        el.message({
-          id: msg.id,
-          values: [7]
-        });
-      });
-
-      var c = m.makeContext();
-      var sum = c.add(3, 4);
-      m.commit(sum, function(error, sum) {
-        assert.equal(error, null);
-        assert.equal(sum, 7);
-        done();
-      });
-    });
-  });
-
-  describe('types', function() {
-    it('should allow the creation of pointer types', function() {
-      assert.equal(t.foo, undefined);
-      m.makePointerType(1000, 'foo', t.int32$);
-      assert(!t.foo.equals(t.int32$));
-      assert(t.foo.baseType.equals(t.int32$));
-      assert(t.foo.id, 1000);
-    });
-
-    it('should allow the creation of struct types', function() {
-      assert.equal(t.foo, undefined);
-      m.makeStructType(1000, 'foo', 8, {
-        'field1': {type: t.char, offset: 0},
-        'field2': {type: t.float32, offset: 4},
-      });
-      assert.equal(Object.keys(t.foo.fields).length, 2);
-      assert.equal(t.foo.size, 8);
-      assert(t.foo.fields.field1.type.equals(t.char));
-      assert.equal(t.foo.fields.field1.offset, 0);
-      assert(t.foo.fields.field2.type.equals(t.float32));
-      assert.equal(t.foo.fields.field2.offset, 4);
-      assert.equal(t.foo.id, 1000);
-    });
-
-    it('should allow the creation of alias types', function() {
-      assert.equal(t.foo, undefined);
-      m.makeAliasType('foo', t.int32);
-      assert(t.foo.equals(t.int32));
-    });
-
-    it('should allow the creation of function types', function() {
-      // Define a function with signature void(float, char*);
-      // The signature must be unique; that is, two function types cannot have
-      // the same signature but different ids.
-      // So we pick a signature that is unlikely to conflict with builtins.
-      // TODO(binji): better way to do this?
-      var ftype = m.makeFunctionType(1000, t.void, t.float32, t.char$);
-      assert.equal(ftype.id, 1000);
-      assert(ftype.retType.equals(t.void));
-      assert.equal(ftype.argTypes.length, 2);
-      assert(ftype.argTypes[0].equals(t.float32));
-      assert(ftype.argTypes[1].equals(t.char$));
-    });
-
-    it('should allow the creation of functions', function() {
-      assert.equal(m.functions.foo, undefined);
-      var ftype = m.makeFunctionType(1000, t.void, t.float32, t.char$);
-      m.makeFunction('foo', ftype);
-      assert.equal(typeof m.functions.foo, 'function');
-    });
-
-    it('should allow the creation of functions w/ overloads', function() {
-      assert.equal(m.functions.foo, undefined);
-      var ftype = m.makeFunctionType(1000, t.void, t.float32, t.char$);
-      var ftype2 = m.makeFunctionType(1001, t.void, t.float64, t.int32$);
-      m.makeFunction('foo', [ftype, ftype2]);
-      assert.equal(typeof m.functions.foo, 'function');
+    assert.deepEqual(m.$getMessage(), {
+      handles: { 1: 4 },
+      commands: [
+        {id: 1, args: [1], ret: 2},
+        {id: 2, args: [2], ret: 3}
+      ]
     });
   });
 });
-*/
