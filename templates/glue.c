8640627876abb5cb0105891c87aeb3f69c29b60f
Ben Smith <binji@chromium.org>
2014-10-01 17:29:57 -0700

    Print error messages when a command fails

--- templates/glue.c
+++ templates/glue.c
@@ -48,6 +48,7 @@ static bool nb_command_run_{{fn.spelling}}(struct Message* message, int command_
 [[  arguments = list(fn.type.argument_types())]]
   int arg_count = nb_message_command_arg_count(message, command_idx);
   if (arg_count != {{len(arguments)}}) {
+    VERROR("Expected %d args, got %d.", {{len(arguments)}}, arg_count);
     return FALSE;
   }
 [[  for i, arg in enumerate(arguments):]]
@@ -58,13 +59,13 @@ static bool nb_command_run_{{fn.spelling}}(struct Message* message, int command_
 [[      if pointee.kind in (TypeKind.CHAR_S, TypeKind.CHAR_U):]]
   char* arg{{i}};
   if (!nb_handle_get_charp(handle{{i}}, &arg{{i}})) {
-    // print error.
+    VERROR("Unable to get handle %d as char*.", handle{{i}});
     return FALSE;
   }
 [[      elif pointee.kind == TypeKind.VOID:]]
   void* arg{{i}};
   if (!nb_handle_get_voidp(handle{{i}}, &arg{{i}})) {
-    // print error.
+    VERROR("Unable to get handle %d as void*.", handle{{i}});
     return FALSE;
   }
 [[      elif pointee.kind == TypeKind.FUNCTIONPROTO:]]
@@ -73,71 +74,71 @@ static bool nb_command_run_{{fn.spelling}}(struct Message* message, int command_
 [[      else:]]
   void* arg{{i}}x;
   if (!nb_handle_get_voidp(handle{{i}}, &arg{{i}}x)) {
-    // print error.
+    VERROR("Unable to get handle %d as void*.", handle{{i}});
     return FALSE;
   }
   {{arg.spelling}}* arg{{i}} = ({{arg.spelling}}*) arg{{i}}x;
 [[    elif arg.kind == TypeKind.LONG:]]
   int32_t arg{{i}}x;
   if (!nb_handle_get_int32(handle{{i}}, &arg{{i}}x)) {
-    // print error.
+    VERROR("Unable to get handle %d as int32_t.", handle{{i}});
     return FALSE;
   }
   long arg{{i}} = (long) arg{{i}}x;
 [[    elif arg.kind == TypeKind.ULONG:]]
   uint32_t arg{{i}}x;
   if (!nb_handle_get_uint32(handle{{i}}, &arg{{i}}x)) {
-    // print error.
+    VERROR("Unable to get handle %d as uint32_t.", handle{{i}});
     return FALSE;
   }
   unsigned long arg{{i}} = (unsigned long) arg{{i}}x;
 [[    elif arg.kind == TypeKind.LONGLONG:]]
   int64_t arg{{i}};
   if (!nb_handle_get_int64(handle{{i}}, &arg{{i}})) {
-    // print error.
+    VERROR("Unable to get handle %d as int64_t.", handle{{i}});
     return FALSE;
   }
 [[    elif arg.kind == TypeKind.ULONGLONG:]]
   uint64_t arg{{i}};
   if (!nb_handle_get_uint64(handle{{i}}, &arg{{i}})) {
-    // print error.
+    VERROR("Unable to get handle %d as uint64_t.", handle{{i}});
     return FALSE;
   }
 [[    elif arg.kind in (TypeKind.INT, TypeKind.SHORT, TypeKind.SCHAR, TypeKind.CHAR_S):]]
   int32_t arg{{i}};
   if (!nb_handle_get_int32(handle{{i}}, &arg{{i}})) {
-    // print error.
+    VERROR("Unable to get handle %d as int32_t.", handle{{i}});
     return FALSE;
   }
 [[    elif arg.kind in (TypeKind.UINT, TypeKind.USHORT, TypeKind.UCHAR, TypeKind.CHAR_U):]]
   uint32_t arg{{i}};
   if (!nb_handle_get_uint32(handle{{i}}, &arg{{i}})) {
-    // print error.
+    VERROR("Unable to get handle %d as uint32_t.", handle{{i}});
     return FALSE;
   }
 [[    elif arg.kind == TypeKind.FLOAT:]]
   float arg{{i}};
   if (!nb_handle_get_float(handle{{i}}, &arg{{i}})) {
-    // print error.
+    VERROR("Unable to get handle %d as float.", handle{{i}});
     return FALSE;
   }
 [[    elif arg.kind == TypeKind.DOUBLE:]]
   double arg{{i}};
   if (!nb_handle_get_double(handle{{i}}, &arg{{i}})) {
-    // print error.
+    VERROR("Unable to get handle %d as double.", handle{{i}});
     return FALSE;
   }
 [[    elif arg.kind == TypeKind.ENUM:]]
   int32_t arg{{i}}x;
   if (!nb_handle_get_int32(handle{{i}}, &arg{{i}}x)) {
-    // print error.
+    VERROR("Unable to get handle %d as int32_t.", handle{{i}});
     return FALSE;
   }
   {{arg.spelling}} arg{{i}} = ({{arg.spelling}}) arg{{i}}x;
 [[    elif arg.kind == TypeKind.RECORD and arg.spelling == 'struct PP_Var':]]
   struct PP_Var arg{{i}};
   if (!nb_handle_get_var(handle{{i}}, &arg{{i}})) {
-    // print error.
+    VERROR("Unable to get handle %d as struct PP_Var.", handle{{i}});
     return FALSE;
   }
 [[    elif arg.kind == TypeKind.CONSTANTARRAY:]]
@@ -152,41 +153,47 @@ static bool nb_command_run_{{fn.spelling}}(struct Message* message, int command_
 [[  result_type = fn.type.get_result().get_canonical()]]
 [[  if result_type.kind != TypeKind.VOID:]]
   if (!nb_message_command_has_ret(message, command_idx)) {
-    // print error.
+    ERROR("Return type is non-void, but no return handle given.");
     return FALSE;
   }
   Handle ret = nb_message_command_ret(message, command_idx);
   {{result_type.spelling}} result = {{fn.spelling}}({{', '.join('arg%d' % i for i in range(len(arguments)))}});
 [[    if result_type.kind in (TypeKind.SCHAR, TypeKind.CHAR_S):]]
-  return nb_handle_register_int8(ret, result);
+  bool register_ok = nb_handle_register_int8(ret, result);
 [[    elif result_type.kind in (TypeKind.UCHAR, TypeKind.CHAR_U):]]
-  return nb_handle_register_uint8(ret, result);
+  bool register_ok = nb_handle_register_uint8(ret, result);
 [[    elif result_type.kind == TypeKind.SHORT:]]
-  return nb_handle_register_int16(ret, result);
+  bool register_ok = nb_handle_register_int16(ret, result);
 [[    elif result_type.kind == TypeKind.USHORT:]]
-  return nb_handle_register_uint16(ret, result);
+  bool register_ok = nb_handle_register_uint16(ret, result);
 [[    elif result_type.kind == TypeKind.INT:]]
-  return nb_handle_register_int32(ret, result);
+  bool register_ok = nb_handle_register_int32(ret, result);
 [[    elif result_type.kind == TypeKind.UINT:]]
-  return nb_handle_register_uint32(ret, result);
+  bool register_ok = nb_handle_register_uint32(ret, result);
 [[    elif result_type.kind == TypeKind.LONG:]]
-  return nb_handle_register_int32(ret, (int32_t)result);
+  bool register_ok = nb_handle_register_int32(ret, (int32_t)result);
 [[    elif result_type.kind == TypeKind.ULONG:]]
-  return nb_handle_register_uint32(ret, (uint32_t)result);
+  bool register_ok = nb_handle_register_uint32(ret, (uint32_t)result);
 [[    elif result_type.kind == TypeKind.LONGLONG:]]
-  return nb_handle_register_int64(ret, result);
+  bool register_ok = nb_handle_register_int64(ret, result);
 [[    elif result_type.kind == TypeKind.ULONGLONG:]]
-  return nb_handle_register_uint64(ret, result);
+  bool register_ok = nb_handle_register_uint64(ret, result);
 [[    elif result_type.kind == TypeKind.FLOAT:]]
-  return nb_handle_register_float(ret, result);
+  bool register_ok = nb_handle_register_float(ret, result);
 [[    elif result_type.kind == TypeKind.DOUBLE:]]
-  return nb_handle_register_double(ret, result);
+  bool register_ok = nb_handle_register_double(ret, result);
 [[    elif result_type.kind == TypeKind.POINTER:]]
-  return nb_handle_register_voidp(ret, result);
+  bool register_ok = nb_handle_register_voidp(ret, result);
 [[    elif result_type.kind == TypeKind.RECORD and result_type.spelling == 'struct PP_Var':]]
-  return nb_handle_register_var(ret, result);
+  bool register_ok = nb_handle_register_var(ret, result);
 [[    else:]]
   // UNSUPPORTED: {{result_type.kind}} {{result_type.spelling}}
+[[    ]]
+  if (!register_ok) {
+    VERROR("Failed to register handle %d of type {{result_type.spelling}}.", ret);
+    return FALSE;
+  }
+  return TRUE;
 [[  else:]]
   {{fn.spelling}}({{', '.join('arg%d' % i for i in range(len(arguments)))}});
   return TRUE;
@@ -210,6 +217,7 @@ static nb_command_func_t s_functions[] = {
 bool nb_message_command_run(struct Message* message, int command_idx) {
   int function_idx = nb_message_command_function(message, command_idx);
   if (function_idx < 0 || function_idx > NUM_FUNCTIONS) {
+    VERROR("Function id %d is out of range [0, %d].", function_idx, NUM_FUNCTIONS);
     return FALSE;
   }
 
